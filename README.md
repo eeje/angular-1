2af8b1e3d7c1fec1448c132351fc6dbbe8ccf14c
eeje <angularyuni@gmail.com>
2022-06-08 17:15:04 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-zdwqzj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-zdwqzj)
---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'inventory-app-root',
+  template: `
+  <h3 
+  [ngStyle]="{
+    'color':budget<=1000?'red':'black',
+    'background-color':budget<=1000?'yellow':'transparent'}">
+      My Budget: &#8361;{{budget}}</h3>
+          <div class="inventory-app">
+            (Products will go here soon)</div>`,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+  budget = 1500;
+}
