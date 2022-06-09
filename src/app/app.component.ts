b969718ba2061e43e2de5f724c37fc6ae2f9553d
eeje <angularyuni@gmail.com>
2022-06-09 15:06:12 +0900

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'inventory-app-root',
   template: `
-  <h3 
-  [ngStyle]="{
-    'color':budget<=1000?'red':'black',
-    'background-color':budget<=1000?'yellow':'transparent'}">
-      My Budget: &#8361;{{budget}}</h3>
+          <h3 
+          [ngStyle]="{
+            'color':budget<=1000?'red':'black',
+            'background-color':budget<=1000?'yellow':'transparent'}">
+              My Budget: &#8361;{{budget}}
+          </h3>
           <div class="inventory-app">
             (Products will go here soon)</div>`,
   styleUrls: ['./app.component.css'],
